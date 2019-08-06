d36d455050cc5613333f0f22c9095e304792551b
liukluke <luca.manfredi1987@gmail.com>
2019-08-06 12:38:14 +0200

    fix server

--- react/phone-catalogue/server/app.js
+++ react/phone-catalogue/server/app.js
@@ -4,21 +4,10 @@ const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
 const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 const cors         = require('cors');
 
-
-mongoose
-  .connect(process.env.DB_LOCAL, {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -42,8 +31,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Express View engine setup
 
+// Express View engine setup
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
@@ -62,12 +51,8 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.locals.title = 'Express - Generated with IronGenerator';
 
 
-
 const index = require('./routes/index');
 app.use('/', index);
-
-const phoneRoutes = require('./routes/phone');
-app.use('/api', phoneRoutes);
 
 
 module.exports = app;
--- react/phone-catalogue/server/routes/index.js
+++ react/phone-catalogue/server/routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router  = express.Router();
+const Phone = require("../phones.json");
 
-/* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get('/phones', (req, res, next) => {
+    res.status(200).json(Phone)
 });
 
 module.exports = router;
