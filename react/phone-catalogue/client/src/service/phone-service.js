e3b9c6087b41ad2a90dc60fee6473bc68dda106b
liukluke <luca.manfredi1987@gmail.com>
2019-08-08 11:17:35 +0200

    dockerize done

--- react/phone-catalogue/client/src/service/phone-service.js
+++ react/phone-catalogue/client/src/service/phone-service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default class phoneService {
 
     static findAll() {
-        return axios.get(`${process.env.REACT_APP_URL}/phones`)
+        return axios.get(`http://localhost:5000/phones`)
             .then(response => response.data)
             .catch(err => console.log(err))
     }
